eddf7df487523b92059c7dc2eaab7f04ed08f3f8
Alvaro-SP <socop2412@gmail.com>
2021-08-16 17:37:14 -0600

    creating second commit

--- Código Fuente/thenodo.py
+++ Código Fuente/thenodo.py
@@ -5,44 +5,44 @@ class thenodo():
     def __str__(self):
         return str(self.value)
 
-graphtext=""
-ene=5
-eme=0
-ac=0
-st=1
-st2=1
-graphtext3=""
-graphtext+="rank=same {"
-for l in range(25):
-    if int(0)==int(0):
+# graphtext=""
+# ene=5
+# eme=0
+# ac=0
+# st=1
+# st2=1
+# graphtext3=""
+# graphtext+="rank=same {"
+# for l in range(25):
+#     if int(0)==int(0):
 
-        if ac<int(ene):
-            ac +=1
+#         if ac<int(ene):
+#             ac +=1
             
-            graphtext+=str(l+1)+"--"
-            graphtext3+=str(st+eme)+"--"
-            st+=eme
-            eme=ene  
-            # graphtext2+="<TD border=\"3\"  bgcolor=\"green:cyan\" gradientangle=\"315\">"+str(gas[l])+"</TD>\n"
-        else:
-            ac=1
-            st2+=1
-            st=st2
-            graphtext=graphtext.rstrip(graphtext[-1])
-            graphtext3=graphtext3.rstrip(graphtext3[-1])
+#             graphtext+=str(l+1)+"--"
+#             graphtext3+=str(st+eme)+"--"
+#             st+=eme
+#             eme=ene  
+#             # graphtext2+="<TD border=\"3\"  bgcolor=\"green:cyan\" gradientangle=\"315\">"+str(gas[l])+"</TD>\n"
+#         else:
+#             ac=1
+#             st2+=1
+#             st=st2
+#             graphtext=graphtext.rstrip(graphtext[-1])
+#             graphtext3=graphtext3.rstrip(graphtext3[-1])
 
-            graphtext+="}\n rank=same {"
-            graphtext+=str(l+1)+"--"
+#             graphtext+="}\n rank=same {"
+#             graphtext+=str(l+1)+"--"
             
-            graphtext3+="\n"+str(st)+"--"
+#             graphtext3+="\n"+str(st)+"--"
             
            
             
             
             
-            # graphtext2+="</TR>\n<TR>"
-            # graphtext2+="<TD border=\"3\"  bgcolor=\"green:cyan\" gradientangle=\"315\">"+str(gas[l])+"</TD>\n"
-graphtext=graphtext.rstrip(graphtext[-1]) 
-graphtext+="}"
-print(graphtext)   
-print(graphtext3)  
+#             # graphtext2+="</TR>\n<TR>"
+#             # graphtext2+="<TD border=\"3\"  bgcolor=\"green:cyan\" gradientangle=\"315\">"+str(gas[l])+"</TD>\n"
+# graphtext=graphtext.rstrip(graphtext[-1]) 
+# graphtext+="}"
+# print(graphtext)   
+# print(graphtext3)  
